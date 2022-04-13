b997763b211b190921a84563c7d29004891c6c96
kanelu <kanelu@k11.com>
2022-04-13 16:42:43 +0800

    重构3

---
+++ pkg/model/model.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"goblog/pkg/logger"
+
+	"gorm.io/gorm"
+	gormlogger "gorm.io/gorm/logger"
+
+	// GORM 的 MySQL 数据库驱动导入
+	"gorm.io/driver/mysql"
+)
+
+// DB gorm.DB 对象
+var DB *gorm.DB
+
+// ConnectDB 初始化模型
+func ConnectDB() *gorm.DB {
+
+	var err error
+
+	config := mysql.New(mysql.Config{
+		DSN: "bibbyee:77499981@tcp(127.0.0.1:3306)/go?charset=utf8&parseTime=True&loc=Local",
+	})
+
+	// 准备数据库连接池
+	DB, err = gorm.Open(config, &gorm.Config{
+		Logger: gormlogger.Default.LogMode(gormlogger.Warn),
+	})
+
+	logger.LogError(err)
+
+	return DB
+}
--- pkg/route/router.go
+++ pkg/route/router.go
@@ -6,17 +6,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Router 路由对象
-var Router *mux.Router
+var route *mux.Router
 
-// Initialize 初始化路由
-func Initialize() {
-	Router = mux.NewRouter()
+func SetRoute(r *mux.Router) {
+	route = r
 }
 
 // Name2URL 通过路由名称来获取 URL
 func Name2URL(routeName string, pairs ...string) string {
-	url, err := Router.Get(routeName).URL(pairs...)
+	url, err := route.Get(routeName).URL(pairs...)
 	if err != nil {
 		// checkError(err)
 		return ""
