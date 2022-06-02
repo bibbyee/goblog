ece51dbb0cec2c67f5d126d7255b28f9e4c186fa
kanelu <kanelu@k11.com>
2022-06-02 16:49:32 +0800

    重建中间件

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
 	"goblog/pkg/database"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -13,29 +13,11 @@ import (
 var router *mux.Router
 var db *sql.DB
 
-func forceHTMLMiddleware(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		h.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	database.Initialize()
 	db = database.DB
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
 
-	router.Use(forceHTMLMiddleware)
-
-	http.ListenAndServe(":2000", removeTrailingSlash(router))
+	http.ListenAndServe(":2000", middlewares.RemoveTrailingSlash(router))
 }
